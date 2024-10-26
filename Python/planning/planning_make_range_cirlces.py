2ca87592fc5049ecd57e97f9919f6f1ac92a2b1c
Michael Studinger <30006192+mstudinger@users.noreply.github.com>
2024-10-26 08:44:54 -0400

    added folders

--- Python/planning/planning_make_range_cirlces.py
+++ Python/planning/planning_make_range_cirlces.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 7, 2023
+Initially created    : Mar  7, 2023
+Restarted development: Oct 25, 2024 
 
-@author: Michael Studinger, NASA Goddard Space Flight Center
+@author: Michael Studinger, NASA - Goddard Space Flight Center
+
+See:
+    https://geospace-code.github.io/pymap3d/vincenty.html#pymap3d.vincenty.vreckon
 
 """
 
@@ -11,67 +15,74 @@ Created on Tue Mar 7, 2023
 # load needed modules
 # =============================================================================
 
-import os, warnings
+import os
+# import warnings
 import numpy as np
 import pymap3d as pm
 import geopandas as gpd
 import pymap3d.vincenty as pmv
 from   shapely.geometry import Polygon
+from   astropy import units as u
+u.imperial.enable()  
 
-warnings.filterwarnings("ignore")  # suppresses all warnings - here used to suppress ShapelyDeprecationWarning from panda module
-
-#%%
 # =============================================================================
-# set airfield, range, and platform
+# set airfield or location, range, and platform
 # =============================================================================
 
-# https://en.wikipedia.org/wiki/Yellowknife_Airport
-# lat_c =   62.463056
-# lon_c = -114.440278
-# airfield = 'CYZF'
+# https://en.wikipedia.org/wiki/Langley_Air_Force_Base
+lat_c    =  37.082778
+lon_c    = -76.360556
+airfield = 'KLFI'  # change to location for cover more applications
+platform = '777'
 
-# https://en.wikipedia.org/wiki/Yakutat_Airport
-lat_c =   59.503333
-lon_c = -139.660278
-airfield = 'PAYA'
+max_range = 3000 * u.imperial.nmi
+print(max_range.value, max_range.unit)
+#max_range = max_range.to(u.m)
+#print(max_range.value, max_range.unit)
 
-platform = 'DHC-6'
-range_km = 500
+#range_km = 3000 * 1.852 # * u.km # an astropy object with distance unit defined: range_km.unit
+# convert to Nautical miles. need to enable imperial submodule first
+
+# range_nm = range_km.to(u.imperial.nmi) # or nauticalmile or NM  
 
 # platform = 'DC-3T'
 # range_km = 900
 
-#%%
 # =============================================================================
 # build output filename based on input parameters
 # =============================================================================
 
-if os.path.exists(r"C:\Users\mstuding\OneDrive - NASA\EVS-4\location_map_QGIS_and_data\range_circles"): # my work laptop
-    f_name_dir  = r"C:\Users\mstuding\OneDrive - NASA\EVS-4\location_map_QGIS_and_data\range_circles"
-else:
-    f_name_dir  = r""
+f_name_dir  = r"..\..\data"
 
-f_name_short = platform + '_' + airfield + '_' + str(range_km) + '_km.shp'
-f_name_shp   = os.path.join(f_name_dir,f_name_short)
+#f_name_short = platform + '_' + airfield + '_' + str(max_range.to(u.imperial.nmi).value) + '_km.shp'
+f_name_short = platform + '_' + airfield + '_' + f"{max_range.value:.0f}" + '_' + str(max_range.unit) + '.gpkg'
+f_name_gis   = os.path.join(f_name_dir,f_name_short)
+
+import fiona
+fiona.supported_drivers['KML'] = 'rw'
+
 
 #%%
 # =============================================================================
 #  calculate waypoints for range circle on WGS-84 ellipsoid
 # =============================================================================
 
-wgs84 = pm.Ellipsoid.from_name('wgs84')
+wgs84 = pm.Ellipsoid.from_name('wgs84') # units are in meters
 azi = range(0,360,1)
 
-lat, lon = pmv.vreckon(lat_c, lon_c, range_km*1000, azi, ell = wgs84)
+lat, lon = pmv.vreckon(lat_c, lon_c, max_range.to(u.m).value, azi, ell = wgs84)
 
-# need to wrap longitudes to ±180 degrees for exporting geographic coordiantes.
-# 0 to 360 is not supported.
+# need to wrap longitudes to ±180° for exporting geographic coordinates
+# 0° to 360° is not supported
 
 lon = np.mod(lon - 180.0, 360.0) - 180.0
 
+# print(np.min(lat))
+# print(np.max(lon))
+
 #%%
 # =============================================================================
-# prepare GeoDataFrame for shapefile export
+# prepare GeoDataFrame for GIS export
 # =============================================================================
 
 # convert waypoint coordinate arrays to lists needed for geometry field
@@ -84,18 +95,82 @@ polygon_geometry = Polygon(zip(lon_point_list, lat_point_list))
 range_circle_gdf = gpd.GeoDataFrame(index = [0], crs = 'epsg:4326', geometry = [polygon_geometry])
 
 # add attributes (primarily used for labelling in QGIS)
-range_circle_gdf.loc[:, "label"] = platform + ' ' + str(range_km) + ' km out-and-back range'
-range_circle_gdf.loc[:, "range_km"] = range_km
-range_circle_gdf.loc[:, "platform"] = platform
-range_circle_gdf.loc[:, "airfield"] = airfield
+range_circle_gdf.loc[:, "label"] = platform + ' ' + str(max_range.value) + ' nmi out-and-back range'
+range_circle_gdf.loc[:, "range_nmi"] = max_range.value
+range_circle_gdf.loc[:, "range_km"]  = max_range.to(u.km).value
+range_circle_gdf.loc[:, "platform"]  = platform
+range_circle_gdf.loc[:, "location"]  = airfield
 
 #%%
 # =============================================================================
 # export shapefile
 # =============================================================================
 
-range_circle_gdf.to_file(filename = f_name_shp, driver = "ESRI Shapefile")
+# range_circle_gdf.to_file(filename = f_name_shp, driver = "ESRI Shapefile")
+range_circle_gdf.to_file(f_name_gis)
+# range_circle_gdf.to_file(f_name_gis.replace('.gpkg','.kml'), driver='KML')
 
 print("\n\tAircraft  : %s" %(platform))
-print("\tRange [km]: %d" %(range_km))
+print("\tRange [nm]: %d" %(max_range.value))
 print("\tSaved file: %s" %(f_name_short))
+
+
+#%% plot search polygon with OpenStreetMap as basemap adn save as HTML file
+
+# https://python-visualization.github.io/folium/latest/user_guide/vector_layers/circle_and_circle_marker.html
+
+import folium
+
+f_name_dir  = r"..\..\html"
+m = range_circle_gdf.explore()
+
+folium.Marker(location=[lat_c, lon_c], popup=airfield).add_to(m)
+
+# folium.Marker(
+#     location=[45.3288, -121.6625],
+#     tooltip="Click me!",
+#     popup="Mt. Hood Meadows",
+#     icon=folium.Icon(icon="cloud"),
+# ).add_to(m)
+
+# folium.Marker(
+#     location=[45.3311, -121.7113],
+#     tooltip="Click me!",
+#     popup="Timberline Lodge",
+#     icon=folium.Icon(color="green"),
+# ).add_to(m)
+
+radius = 3000
+folium.Circle(
+    location=[lat_c, lon_c],
+    radius=radius,
+    color="black",
+    weight=1,
+    fill_opacity=0.6,
+    opacity=1,
+    fill_color="green",
+    fill=False,  # gets overridden by fill_color
+    popup="{} meters".format(radius),
+    tooltip="I am in meters",
+).add_to(m)
+
+# radius = 10000
+# folium.Circle(
+#     location=[-27.551667, -48.478889],
+#     radius=radius,
+#     color="black",
+#     weight=1,
+#     fill_opacity=0.6,
+#     opacity=1,
+#     fill_color="green",
+#     fill=False,  # gets overridden by fill_color
+#     popup="{} meters".format(radius),
+#     tooltip="I am in meters",
+# ).add_to(m)
+
+
+f_name_html = os.path.join(f_name_dir,"range_map.html")
+m.save(f_name_html)
+
+#import webbrowser
+#webbrowser.open(f_name_html)
