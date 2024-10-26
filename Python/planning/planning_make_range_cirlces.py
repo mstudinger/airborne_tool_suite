c5388871e1e707493d5d37afbf0ec130f5696068
Michael Studinger <30006192+mstudinger@users.noreply.github.com>
2024-10-26 18:40:38 -0400

    added files to ignore and range circle code

--- Python/planning/planning_make_range_cirlces.py
+++ Python/planning/planning_make_range_cirlces.py
@@ -5,18 +5,14 @@ Restarted development: Oct 25, 2024
 
 @author: Michael Studinger, NASA - Goddard Space Flight Center
 
-See:
-    https://geospace-code.github.io/pymap3d/vincenty.html#pymap3d.vincenty.vreckon
+Purpose: create GeoDataFrame with range circle and return format independent GIS file name
+See: https://geospace-code.github.io/pymap3d/vincenty.html#pymap3d.vincenty.vreckon
 
 """
 
-#%%
-# =============================================================================
 # load needed modules
-# =============================================================================
 
 import os
-# import warnings
 import numpy as np
 import pymap3d as pm
 import geopandas as gpd
@@ -25,152 +21,89 @@ from   shapely.geometry import Polygon
 from   astropy import units as u
 u.imperial.enable()  
 
-# =============================================================================
-# set airfield or location, range, and platform
-# =============================================================================
 
-# https://en.wikipedia.org/wiki/Langley_Air_Force_Base
-lat_c    =  37.082778
-lon_c    = -76.360556
-airfield = 'KLFI'  # change to location for cover more applications
-platform = '777'
+# define function
 
-max_range = 3000 * u.imperial.nmi
-print(max_range.value, max_range.unit)
-#max_range = max_range.to(u.m)
-#print(max_range.value, max_range.unit)
+def range_to_gdf(
+    lon_o:float,     # longitude of origin in decimal degrees
+    lat_o:float,     # latitude  of origin in decimal degrees
+    location:str,    # location name. e.g., airfield code such as "KLFI"
+    platform:str,    # name of platform. e.g. "777", "DHC-6"
+    max_range:float, # platform max_range. will be converted to out-and-back range.
+    range_unit:str,  # distance unit for range. must be "nmi" or "km".
+    ) -> object:     # GeoDataFrame with range circle and metadata and filename
 
-#range_km = 3000 * 1.852 # * u.km # an astropy object with distance unit defined: range_km.unit
-# convert to Nautical miles. need to enable imperial submodule first
+    """
+    Purpose     : create GeoDataFrame with range circle and return format independent GIS file name (no extension)
+    Usage       : gdf, f_name = range_to_gdf(-76.360556,37.082778,'KLFI','777',3000,"nmi")
+    Dependencies: os, numpy, pymap3d, geopandas, shapely, astropy
+    """
+    
+    # check if critical input arguments are valid
+    
+    # 1) verify latitude of origin
+    if (isinstance(float(lat_o), float) and (-90 <= lat_o <= 90)) == False:
+        os.sys.exit("longitude of origin must be of type float and in decimal degrees (±90°)")
+        
+    # 2) verify longitude of origin
+    # first wrap longitude to ±180°
+    lon_o = np.mod(lon_o - 180.0, 360.0) - 180.0 
+    if (isinstance(float(lon_o), float) and (-180 <= lon_o <= 180)) == False:
+        os.sys.exit("longitude of origin must be of type float and in decimal degrees (±180°)")
+        
+    # 3) verify range_unit
+    if (range_unit == "nmi" or range_unit == "km") == False:
+        os.sys.exit("range_unit must be either 'nmi' or 'km'")
+        
+    # 4) verify max_range and convert to astropy object with range_unit
+    if isinstance(float(max_range), float) and max_range > 0:
+        if range_unit == "nmi":
+            half_range = 0.5 * max_range * u.imperial.nmi
+        elif range_unit == "km":
+            half_range = 0.5 * max_range * u.imperial.km            
+        # convert to meters for waypoint calculation
+        half_range = half_range.to(u.m)        
+    else:    
+        os.sys.exit("max_range must be of type float and > 0")
 
-# range_nm = range_km.to(u.imperial.nmi) # or nauticalmile or NM  
+    # create waypoints for half-range circe on WGS-8 ellipsoid
+    wgs84 = pm.Ellipsoid.from_name('wgs84') # units are in meters
+    azi = range(0,360,1)
+    lat, lon = pmv.vreckon(lat_o, lon_o, half_range.to(u.m).value, azi, ell = wgs84)
+    # need to wrap longitudes to ±180° for exporting geographic coordinates in GeoDataFrame
+    # 0° to 360° is not supported
+    lon = np.mod(lon - 180.0, 360.0) - 180.0
+    
+    # create GeoDataFrame for GIS export
+    # convert waypoint coordinate arrays to lists needed for geometry field
+    lat_point_list = list(lat); lon_point_list = list(lon)
+    
+    # set up geometry field
+    polygon_geometry = Polygon(zip(lon_point_list, lat_point_list))
+    
+    # create GeoDataFrame with coordinate reference system and polygon geometry
+    range_circle_gdf = gpd.GeoDataFrame(index = [0], crs = 'epsg:4326', geometry = [polygon_geometry])
+    
+    # add attributes (primarily used for labelling in QGIS)
+    range_circle_gdf.loc[:, "label"] = platform + ' ' + f"{max_range*0.5:.0f}" + ' ' + range_unit + ' out-and-back range'
+    range_circle_gdf.loc[:, "range_nmi"] = half_range.to(u.imperial.nmi).value
+    range_circle_gdf.loc[:, "range_km"]  = half_range.to(u.km).value
+    range_circle_gdf.loc[:, "platform"]  = platform
+    range_circle_gdf.loc[:, "location"]  = location
+     
+    # build format independent GIS output filename using input parameters
+    f_name_gis = platform + '_' + location + '_' + f"{max_range:.0f}" + '_' + range_unit
+    
+    return range_circle_gdf, f_name_gis
 
-# platform = 'DC-3T'
-# range_km = 900
-
-# =============================================================================
-# build output filename based on input parameters
-# =============================================================================
-
-f_name_dir  = r"..\..\data"
-
-#f_name_short = platform + '_' + airfield + '_' + str(max_range.to(u.imperial.nmi).value) + '_km.shp'
-f_name_short = platform + '_' + airfield + '_' + f"{max_range.value:.0f}" + '_' + str(max_range.unit) + '.gpkg'
-f_name_gis   = os.path.join(f_name_dir,f_name_short)
-
-import fiona
-fiona.supported_drivers['KML'] = 'rw'
-
-
-#%%
-# =============================================================================
-#  calculate waypoints for range circle on WGS-84 ellipsoid
-# =============================================================================
-
-wgs84 = pm.Ellipsoid.from_name('wgs84') # units are in meters
-azi = range(0,360,1)
-
-lat, lon = pmv.vreckon(lat_c, lon_c, max_range.to(u.m).value, azi, ell = wgs84)
-
-# need to wrap longitudes to ±180° for exporting geographic coordinates
-# 0° to 360° is not supported
-
-lon = np.mod(lon - 180.0, 360.0) - 180.0
-
-# print(np.min(lat))
-# print(np.max(lon))
-
-#%%
-# =============================================================================
-# prepare GeoDataFrame for GIS export
-# =============================================================================
-
-# convert waypoint coordinate arrays to lists needed for geometry field
-lat_point_list = list(lat); lon_point_list = list(lon)
-
-# set up geometry field
-polygon_geometry = Polygon(zip(lon_point_list, lat_point_list))
-
-# create GeoDataFrame with coordinate reference system and polygon geometry
-range_circle_gdf = gpd.GeoDataFrame(index = [0], crs = 'epsg:4326', geometry = [polygon_geometry])
-
-# add attributes (primarily used for labelling in QGIS)
-range_circle_gdf.loc[:, "label"] = platform + ' ' + str(max_range.value) + ' nmi out-and-back range'
-range_circle_gdf.loc[:, "range_nmi"] = max_range.value
-range_circle_gdf.loc[:, "range_km"]  = max_range.to(u.km).value
-range_circle_gdf.loc[:, "platform"]  = platform
-range_circle_gdf.loc[:, "location"]  = airfield
-
-#%%
-# =============================================================================
-# export shapefile
-# =============================================================================
-
-# range_circle_gdf.to_file(filename = f_name_shp, driver = "ESRI Shapefile")
-range_circle_gdf.to_file(f_name_gis)
-# range_circle_gdf.to_file(f_name_gis.replace('.gpkg','.kml'), driver='KML')
-
-print("\n\tAircraft  : %s" %(platform))
-print("\tRange [nm]: %d" %(max_range.value))
-print("\tSaved file: %s" %(f_name_short))
-
-
-#%% plot search polygon with OpenStreetMap as basemap adn save as HTML file
-
-# https://python-visualization.github.io/folium/latest/user_guide/vector_layers/circle_and_circle_marker.html
-
-import folium
-
-f_name_dir  = r"..\..\html"
-m = range_circle_gdf.explore()
-
-folium.Marker(location=[lat_c, lon_c], popup=airfield).add_to(m)
-
-# folium.Marker(
-#     location=[45.3288, -121.6625],
-#     tooltip="Click me!",
-#     popup="Mt. Hood Meadows",
-#     icon=folium.Icon(icon="cloud"),
-# ).add_to(m)
-
-# folium.Marker(
-#     location=[45.3311, -121.7113],
-#     tooltip="Click me!",
-#     popup="Timberline Lodge",
-#     icon=folium.Icon(color="green"),
-# ).add_to(m)
-
-radius = 3000
-folium.Circle(
-    location=[lat_c, lon_c],
-    radius=radius,
-    color="black",
-    weight=1,
-    fill_opacity=0.6,
-    opacity=1,
-    fill_color="green",
-    fill=False,  # gets overridden by fill_color
-    popup="{} meters".format(radius),
-    tooltip="I am in meters",
-).add_to(m)
-
-# radius = 10000
-# folium.Circle(
-#     location=[-27.551667, -48.478889],
-#     radius=radius,
-#     color="black",
-#     weight=1,
-#     fill_opacity=0.6,
-#     opacity=1,
-#     fill_color="green",
-#     fill=False,  # gets overridden by fill_color
-#     popup="{} meters".format(radius),
-#     tooltip="I am in meters",
-# ).add_to(m)
-
-
-f_name_html = os.path.join(f_name_dir,"range_map.html")
-m.save(f_name_html)
-
-#import webbrowser
-#webbrowser.open(f_name_html)
+# test function
+if __name__ == '__main__':
+    
+    # https://en.wikipedia.org/wiki/Langley_Air_Force_Base
+    lat_o    =  37.082778
+    lon_o    = -76.360556
+    airfield = 'KLFI'
+    platform = '777'
+    
+    # execute function for testing    
+    gdf, f_name = range_to_gdf(lon_o, lat_o, airfield, platform, 3000, "nmi")
